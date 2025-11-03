b0b87353eb51b36aa1f95e0fb9a15f4a5bba35e9
Joe Marian A <joemarian3010@gmail.com>
2025-11-03 13:00:27 +0530

    main test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/mood', methods=['POST'])
-def set_mood():
-    data = request.json
-    mood = data.get('mood', '')
-    return jsonify({'success': True, 'mood': mood})
+@app.route('/')
+def home():
+    return """
+    <div style='font-family: Arial; text-align: center; margin-top: 100px;'>
+        <h2>🚀 Backend Successfully Deployed on EC2! test 2</h2>
+        <p>This Flask API is running inside a Docker container managed via Jenkins CI/CD.</p>
+        <p>Check <a href='/status'>/status</a> for API health.</p>
+    </div>
+    """
 
 @app.route('/status', methods=['GET'])
 def status():
-    return jsonify({'status': 'API running'})
+    return jsonify({'status': 'API is up and running!', 'service': 'Flask Backend'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
